994bfe2b43b4d02d76ba112bd22607e5b5ae6b37
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-07 12:05:14 +0200

    nav bar logo through container

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,21 +3,21 @@
     <div class="" >
         <div class="menu-content">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/"><img src="{{asset('logo/logo.png')}}" alt="" width="120" height="120" style="margin-top:-40px; "></a>
-            </div>
+ 
 
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 <div class="container">
-    
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/"><img src="{{asset('logo/logo.png')}}" alt="" width="120" height="120" style="margin-top:-40px; "></a>
+    </div>
 
                 <ul class="nav navbar-nav text-uppercase">
                     <li><a href="/">Homepage</a></li>
